a767c4cb1e35917200b7fa8067b56f1dd059058f
Chingis Sandanov <chingis@chingis.io>
2025-11-21 16:29:29 +0800

    Bugfix: latest tag detected incorrectly, fixes #2

--- .github/actions/release.sh
+++ .github/actions/release.sh
@@ -8,17 +8,17 @@ if [[ "${GITHUB_REF}" == refs/heads/master || "${GITHUB_REF}" == refs/tags/* ]];
   major_tag="${minor_ver%.*}"
 
   tags=("${minor_tag}")
-  if [[ -n "${LATEST_MAJOR}" ]]; then
+  if [[ "${LATEST_MAJOR}" == "true" ]]; then
      tags+=("${major_tag}")
   fi
 
   if [[ "${GITHUB_REF}" == refs/tags/* ]]; then
-    stability_tag=("${GITHUB_REF##*/}")
+    stability_tag="${GITHUB_REF##*/}"
     tags=("${minor_tag}-${stability_tag}")
     if [[ -n "${LATEST_MAJOR}" ]]; then
       tags+=("${major_tag}-${stability_tag}")
     fi
-  elif [[ -n "${LATEST}" ]]; then
+  elif [[ "${LATEST}" == "true" ]]; then
     tags+=("latest")
   fi
 
